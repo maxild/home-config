86b8e9d24efdd977291adcd49fd4d487204feae5
mmaxild@gmail.com <maxild>
2020-04-15 10:30:59 +0200

    Added nix-prefetch-git and nix-prefetch-github to user environment

--- dev.nix
+++ dev.nix
@@ -4,7 +4,7 @@
   imports = [
     #./dev/emacs.nix
     ./dev/git.nix
-    #./dev/nix.nix
+    ./dev/nix.nix
   ];
 
   home.file.".editorconfig".source = ./. + "/dotfiles/root.editorconfig";
---
+++ dev/nix.nix
@@ -0,0 +1,12 @@
+{ pkgs, ... }:
+
+{
+  home.packages = with pkgs; [
+    nix-prefetch-git
+    nix-prefetch-github
+    #nodePackages.node2nix
+    #go2nix
+    #nixops
+    #cachix
+  ];
+}
