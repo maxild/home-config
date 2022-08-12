0d2e8df765f3b4b341a6f9217719a2f0b1e16440
maxild <mmaxild@gmail.com>
2022-08-12 15:21:46 +0200

    Updated idea-keymaps script to 2022.2

--- scripts/idea-keymaps.ps1
+++ scripts/idea-keymaps.ps1
@@ -6,7 +6,7 @@ Param(
     [string]$Platform = "Windows",
     [ValidateSet("Rider", "GoLand", "CLion", "IntelliJIdea", "PyCharm", "WebStorm")]
     [string]$Product = "Rider",
-    [string]$Version = "2022.1"
+    [string]$Version = "2022.2"
 )
 
 $ScriptsPath = split-path -Parent $MyInvocation.MyCommand.Definition
