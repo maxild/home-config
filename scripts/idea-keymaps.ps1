fe9a7053e0a0a995eef214bebc31ba2fe3eb3620
maxild <mmaxild@gmail.com>
2023-12-18 12:12:45 +0100

    Updated to Rider2023.3

--- scripts/idea-keymaps.ps1
+++ scripts/idea-keymaps.ps1
@@ -6,7 +6,7 @@ Param(
     [string]$Platform = "Windows",
     [ValidateSet("Rider", "GoLand", "CLion", "IntelliJIdea", "PyCharm", "WebStorm")]
     [string]$Product = "Rider",
-    [string]$Version = "2023.2"
+    [string]$Version = "2023.3"
 )
 
 $ScriptsPath = split-path -Parent $MyInvocation.MyCommand.Definition
